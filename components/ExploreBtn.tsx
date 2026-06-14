42d81f3bdaace5ae7d66ac096de1e6d638992bb8
Anastasiia Boiko <nastya.boiko@scrumlaunch.com>
2026-06-14 10:59:53 +0200

    Refactor components for improved readability and consistency

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,17 +1,27 @@
 'use client';
 
-import Image from "next/image";
-import posthog from "posthog-js";
+import Image from 'next/image';
+import posthog from 'posthog-js';
 
 const ExploreBtn = () => {
-    return (
-        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => posthog.capture('explore_events_clicked')}>
-            <a href="#events">
-                Explore Events
-                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
-            </a>
-        </button>
-    )
-}
+  return (
+    <button
+      type='button'
+      id='explore-btn'
+      className='mt-7 mx-auto'
+      onClick={() => posthog.capture('explore_events_clicked')}
+    >
+      <a href='#events'>
+        Explore Events
+        <Image
+          src='/icons/arrow-down.svg'
+          alt='arrow-down'
+          width={24}
+          height={24}
+        />
+      </a>
+    </button>
+  );
+};
 
-export default ExploreBtn
+export default ExploreBtn;
